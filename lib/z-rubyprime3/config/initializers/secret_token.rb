3c1fc2d68f40b5d9e87bc121c4d1142ed44156ba
John Stafford <johnlstafford@gmail.com>
2014-01-30 11:26:34 +1100

    Add Pins Scaffold

--- lib/z-rubyprime3/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rubyprime::Application.config.secret_token = '547ca6731fb69ad27510cf1a4e283f273f7557b746191810f9b31c38b1716ea88ba1da92c290f36f18545b1e928674e73db7d1694ab385033104a8d7342e99a2'
